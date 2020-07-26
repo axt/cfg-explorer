471c60d0c7abb6431f0f2a7380427ada1a2c18ad
Xinyi Li <wolixinyi@gmail.com>
2020-07-26 08:07:47 -0400

    doc strings

--- cfgexplorer/utils.py
+++ cfgexplorer/utils.py
@@ -9,12 +9,23 @@ from .endpoint import CFGVisEndpoint, FGraphVisEndpoint
 from networkx.drawing.nx_agraph import write_dot
 
 
-def cfg_explore(binary,
-                starts=[],
-                port=5050,
-                pie=False,
-                lanuch=False,
-                output=''):
+def cfg_explore(binary, starts=[], port=5050, pie=False, launch=False, output=''):
+    """
+    :param binary: the path of binary file for analysis
+    :type binary: str
+    :param starts: the start points (address) in CFGs, if none, the CFG will start with main func entry address
+    :type starts: list
+    :param port: server port to host the web app. make sure the port is idle now.
+    :type port: int
+    :param pie: whether the analysis position independent
+    :type pie: bool
+    :param launch: Launch a browser to view CFG immediately
+    :type launch: bool
+    :param output: the output file path. only support for '.dot' and '.svg' now. If leave it an empty string, no output will be generated and the interactive web app will start. Otherwise, no app will be launched and the CFGs will be exported to specified files.
+    :type output: str
+    :return: None
+    :rtype: None
+    """
     main_opts = {}
     if pie:
         main_opts['custom_base_addr'] = 0x0
@@ -23,30 +34,18 @@ def cfg_explore(binary,
     # create CFG
     if starts:
         addrs = get_addrs(proj, starts)
-        cfg = proj.analyses.CFGFast(fail_fast=False,
-                                    normalize=True,
-                                    show_progressbar=True,
-                                    symbols=False,
-                                    function_prologues=False,
-                                    force_complete_scan=False,
-                                    collect_data_references=False,
-                                    start_at_entry=False,
-                                    function_starts=addrs,
-                                    resolve_indirect_jumps=True)
+        cfg = proj.analyses.CFGFast(fail_fast=False, normalize=True, show_progressbar=True, symbols=False,
+                                    function_prologues=False, force_complete_scan=False, collect_data_references=False,
+                                    start_at_entry=False, function_starts=addrs, resolve_indirect_jumps=True)
     else:
-        cfg = proj.analyses.CFGFast(fail_fast=False,
-                                    normalize=True,
-                                    show_progressbar=True,
-                                    symbols=True,
-                                    function_prologues=True,
-                                    force_complete_scan=True,
-                                    collect_data_references=False,
+        cfg = proj.analyses.CFGFast(fail_fast=False, normalize=True, show_progressbar=True, symbols=True,
+                                    function_prologues=True, force_complete_scan=True, collect_data_references=False,
                                     resolve_indirect_jumps=True)
         addrs = get_addrs(proj, starts)
 
     # lanuch a flask app
     if not output:
-        lanuch_app(lanuch, port)
+        lanuch_app(launch, port)
         app = CFGExplorer(start_url='/api/cfg/%#08x' % addrs[0], port=port)
         app.add_vis_endpoint(CFGVisEndpoint('cfg', cfg))
         app.add_vis_endpoint(FGraphVisEndpoint('function', proj, cfg))
