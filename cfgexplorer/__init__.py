324fc2ff411786df62471f9166ad18e5c5379e0c
Hosein Yavarzadeh <h.yavarzadeh274@gmail.com>
2023-07-29 11:26:09 -0700

    Dev: Add a new 'cfg_explore' function to get cfg and proj as inputs

--- cfgexplorer/__init__.py
+++ cfgexplorer/__init__.py
@@ -1,4 +1,4 @@
 from .endpoint import VisEndpoint, CFGVisEndpoint, FGraphVisEndpoint
 from .explorer import CFGExplorer
 from .cli import CFGExplorerCLI
-from .utils import cfg_explore
+from .utils import cfg_explore, cfg_explore_get_cfg
